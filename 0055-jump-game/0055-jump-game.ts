0108a7aea958c4f60f5ea4fcc212838a55cfb87d
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-09-19 22:13:22 +0900

    Time: 65 ms (73.66%), Space: 46.4 MB (99.15%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.ts
@@ -0,0 +1,12 @@
+function canJump(nums: number[]):boolean{
+
+    let left = nums[0];
+
+    for(let i = 1; i<nums.length; i++){
+        if(left === 0){
+            return false;
+        }
+        left = Math.max(left-1, nums[i]);
+    }
+    return true;
+};

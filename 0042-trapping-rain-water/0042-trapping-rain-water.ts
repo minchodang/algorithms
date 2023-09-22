05762ef27fc5124b0d171ce3d3b3a0eeca348ab7
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-09-22 14:02:30 +0900

    Time: 68 ms (48.93%), Space: 44.7 MB (92.04%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.ts
@@ -0,0 +1,25 @@
+function trap(height: number[]): number {
+ let left = 0, right = height.length - 1;
+    let maxLeft = 0, maxRight = 0;
+    let result = 0;
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            if (height[left] >= maxLeft) {
+                maxLeft = height[left];
+            } else {
+                result += maxLeft - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= maxRight) {
+                maxRight = height[right];
+            } else {
+                result += maxRight - height[right];
+            }
+            right--;
+        }
+    }
+
+    return result;
+};

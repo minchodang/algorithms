5594b4c87c9271e26ddc2036d9a395804d3f54ef
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-10-07 20:25:44 +0900

    Time: 61 ms (21.82%), Space: 42.6 MB (64.49%) - LeetHub

---
+++ 2796-allow-one-function-call/2796-allow-one-function-call.ts
@@ -0,0 +1,19 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    
+    let i = 0;
+	return function (...args) {
+        i +=1;
+        return i === 1 ? fn(...args) : undefined;
+	};
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */

3c5aea80da00f29a7654db1790ad16d7f4186a52
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-10-14 16:56:26 +0900

    Time: 127 ms (72.64%), Space: 56.3 MB (96.62%) - LeetHub

---
+++ 2860-sort-by/2860-sort-by.ts
@@ -0,0 +1,9 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (value: JSONValue) => number
+
+function sortBy(arr: JSONValue[], fn: Fn): JSONValue[] {
+    arr.sort((a,b)=>{
+        return fn(a) - fn(b);
+    })
+	return arr;
+};
